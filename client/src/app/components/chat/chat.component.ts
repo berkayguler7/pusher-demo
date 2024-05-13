7e7e9f3c5f846111df998be38a4a13788f1949bb
BerkiManman <berkayguler7@hotmail.com>
2024-05-13 14:36:57 +0300

    refactor: split pusher service from chat component

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import Pusher from 'pusher-js';
 import env from '../../../environments/environment';
+import { PusherService } from '../../services/pusher.service';
 
 @Component({
   selector: 'app-chat',
@@ -9,23 +9,16 @@ import env from '../../../environments/environment';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
-  apiURL = env.apiURL;
   messages: string[] = [];
   newMessage: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private pusherService: PusherService) {}
 
   ngOnInit(): void {
-    const pusher = new Pusher(env.pusherAppKey, {
-      cluster: env.pusherAppCluster,
-    });
-
-    const channel = pusher.subscribe('chat');
-
-    channel.bind('message', (data: { user: string; message: string }) => {
+    this.pusherService.subscribeToChannel('chat', 'message', (data: { user: string; message: string }) => {
       this.messages.push(`${data.user}: ${data.message}`);
       this.scrollToBottom();
-    });
+    })
 
     const accessToken = document.cookie
       .split(';')
@@ -38,33 +31,9 @@ export class ChatComponent implements OnInit {
   }
 
   sendMessage() {
-    console.log('sendMessage')
-    const message = this.newMessage.trim();
-    if (message !== '') {
-      console.log('sendMessage', message)
-      this.scrollToBottom();
-      this.newMessage = '';
-
-      const accessToken = document.cookie
-        .split(';')
-        .find((cookie) => cookie.includes('accessToken'))
-        ?.split('=')[1] as string;
-
-      console.log('sendMessage', accessToken)
-
-      this.http.post(
-          this.apiURL + '/message',
-          { message },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: accessToken,
-            },
-            withCredentials: true,
-          },
-        )
-        .subscribe();
-    }
+    this.pusherService.sendMessage(this.newMessage.trim());
+    this.newMessage = '';
+    this.scrollToBottom();
   }
 
   scrollToBottom() {
---
+++ client/src/app/services/pusher.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import Pusher, { Channel } from 'pusher-js';
+import env from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PusherService {
+  private pusher: Pusher;
+  private channel: Channel | undefined;
+
+  constructor(private http: HttpClient) {
+    this.pusher = new Pusher(env.pusherAppKey, {
+      cluster: env.pusherAppCluster
+    });
+  }
+
+  subscribeToChannel(channelName: string, eventName: string, callback: Function) {
+    this.channel = this.pusher.subscribe(channelName);
+    this.channel.bind(eventName, callback);
+  }
+
+  sendMessage(message: string) {
+    if (message !== '') {
+      const accessToken = document.cookie
+        .split(';')
+        .find((cookie) => cookie.includes('accessToken'))
+        ?.split('=')[1] as string;
+
+      console.log('message', message)
+      console.log('accessToken', accessToken)
+      console.log('apiURL', env.apiURL)
+      console.log('headers', {
+        'Content-Type': 'application/json',
+        Authorization: accessToken,
+      })
+
+
+      let context = this.http.post(
+          env.apiURL + '/message',
+          { message },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: accessToken,
+            },
+            withCredentials: true,
+          },
+        ).subscribe();
+      console.log('context', context)
+    }
+  }
+}
